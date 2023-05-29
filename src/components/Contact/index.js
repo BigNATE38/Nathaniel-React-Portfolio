199d662e5a231f5ab44b6b1357df0174b03a636b
BigNATE38 <natevandy38@gmail.com>
2023-05-29 04:13:02 -0700

    contact functionality and css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { validEmail, validUser, validMessage  } from '../../utils/helpers';
 
 export default function Contact() {
+
+    const [errMessage, setErrMessage] = useState('');
+
+    const secureValidation = (event) => {
+        if (event.target.name === "name") {
+            if (!validUser(event.target.value)) {
+            setErrMessage("Enter valid name");
+            } else {
+                setErrMessage("");
+            }
+        }
+        if (event.target.name === "email") {
+            if (!validEmail(event.target.value)) {
+                setErrMessage("Enter valid email");
+            } else {
+                setErrMessage("");
+            }
+        }
+        if (event.target.name === "message") {
+            if (!validMessage(event.target.value)) {
+                setErrMessage("Enter valid message");
+            } else {
+                setErrMessage("");
+            }
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(errMessage === ""){
+
+        }
+    }
     return (
         <div>
-             <form className="edit-post-form post-card-position">
+             <form className="edit-post-form post-card-position" onSubmit={handleSubmit}>
         <div class="card post-card-size">
             <h3 className="post-card-new">Contact</h3>
             <div className="card-body">
                 <h5>Name</h5>
-                <input className="title-name" type="text" id="new-post-title" name="new-post-title" />
+                <input className="title-name" type="text" id="new-post-title" name="name" onBlur={secureValidation}></input>
                 <div className="content-space">
                 <h5>Email</h5>
                 </div>
-                <input className="title-name" type="text" id="new-post-title" name="new-post-title"/>
+                <input className="title-name" type="text" id="new-post-title" name="email" onBlur={secureValidation}></input>
                 <div className="content-space">
                     <h5>Message</h5>
-                    <textarea className="content-len" id="new-comment-body" name="new-comment-body"></textarea>
+                    <textarea className="content-len" id="new-comment-body" name="message" onBlur={secureValidation}></textarea>
                 </div>
             </div>
             <div className="card-body">
@@ -23,6 +57,7 @@ export default function Contact() {
             </div>
         </div>
     </form> 
+    {errMessage && <p className='the-error'>{errMessage}</p>}
 
         </div>
     )
